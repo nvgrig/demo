36da779e111cf5f9c555cef6062a9f2e3982e49a
Nikita Grigoryev <nvgrigoryev@gmail.com>
2024-09-05 21:57:47 +0400

    add model

---
+++ src/main/java/ru/nvgrig/demo/model/Corse.java
@@ -0,0 +1,31 @@
+package ru.nvgrig.demo.model;
+
+public class Corse {
+    private Integer id;
+    private String author;
+    private String title;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
